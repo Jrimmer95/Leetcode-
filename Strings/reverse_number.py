8c2e765bba2a8e2ede3eff5c1ca7d5dfc9feea9f
Jordan <jrimmer95@gmail.com>
2019-07-22 13:58:48 +0100

    Initialising repo

---
+++ Strings/reverse_number.py
@@ -0,0 +1,27 @@
+"""
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Note:
+    Assume we are dealing with an environment which could only store integers within the 32-bit signed integer
+    range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed
+    integer overflows.
+
+"""
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 0:
+            x = str(x)
+            y = ''
+            y += (x[::-1])
+            if int(y) > (2**31)-1:
+                return 0
+            return int(y)
+        elif x < 0:
+            x = x * -1
+            x = str(x)
+            y = ''
+            y += (x[::-1])
+            if -int(y) < -(2**31):
+                return 0
+            return -int(y)
